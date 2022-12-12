1ad6a38aaefd3c2b7584c0195987b4672b2fa30d
wajih942 <wajih.jemai@esprit.tn>
2022-12-12 01:08:20 +0100

    update our menu-items to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 
 import './App.css';
 import HomePage from './pages/homepage/homepage.component'
+import { Route,Switch } from 'react-router-dom';
+const HatsPage = () =>(
+  <div>
+    <h1>HATS PAGES</h1>
+  </div>
+  )
 function App() {
   return (
-    <div><HomePage></HomePage></div>
+    <div className='App'>
+      <Switch>
+          <Route exact path='/' component={HomePage}/>
+          <Route path='/hats' component={HatsPage}/>
+      </Switch>  
+    </div>
   );
 }
 
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -10,7 +10,7 @@ class Directory extends Component{
                     title: 'hats',
                     imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
                     id: 1,
-                    linkUrl: 'shop/hats'
+                    linkUrl: 'hats'
                   },
                   {
                     title: 'jackets',
@@ -44,8 +44,8 @@ class Directory extends Component{
     render(){
         return (
             <div className="directory-menu">
-                {this.state.sections.map(({title,imageUrl,id,size})=>{
-                    return <MenuItem key={id} title={title} imageUrl={imageUrl} size={size}></MenuItem>
+                {this.state.sections.map(({title,imageUrl,id,size,linkUrl})=>{
+                    return <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} linkUrl={linkUrl}></MenuItem>
                 })}
             </div>
         )
